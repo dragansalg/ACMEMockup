f95810a96bc0762ed1315dc03ae9f5db390b89ac
Deepthi Ushus <ushusdeepthi@gmail.com>
2020-10-21 09:35:54 +0200

    changes made to css,js and html

--- Webpage/scripts/events.js
+++ Webpage/scripts/events.js
@@ -1,5 +1,15 @@
-fetch('events.json')
+fetch('../scripts/events.json')
 .then(response=>response.json())
 .then(data=>{
-    console.log(data);
+   console.log(data);
 });
+//inuti fetchen
+/*let header=document.getElementById("header");
+let event1=document.createElement("div");
+let heading= document.createElement("h1");
+heading.innerHTML=data[1].name;
+let image= document.createElement("img");
+image.setAttribute('src',data[1].imageLocation);
+event1.appendChild(heading);
+event1.appendChild(image);
+header.after(event1);*/
