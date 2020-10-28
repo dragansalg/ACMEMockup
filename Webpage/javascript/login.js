f1530f8912d6e28d8cc39b72923b2d5b92520662
Erik Somnäs <erik.somnas@gmail.com>
2020-10-28 20:20:17 +0100

    merge main

---
+++ Webpage/javascript/login.js
@@ -0,0 +1,38 @@
+const admins = [
+     {
+          username: "erik",
+          password: "hejsan"
+     },
+     {
+          username: "danne",
+          password: "ciao"
+     },
+     {
+          username: "adde",
+          password: "fritös"
+     },
+]
+
+document.addEventListener("DOMContentLoaded", () => {
+     
+     let button = document.getElementById("button");
+     
+     button.addEventListener("click", (e) => {
+          e.preventDefault();
+          checkUser();          
+     })
+
+     function checkUser() {
+          let input_username = document.getElementById("username").value;
+          let input_password = document.getElementById("password").value;        
+
+          for(let i = 0; i < admins.length; i++) {
+               if(input_username == admins[i].username && input_password == admins[i].password) {
+                    window.location.href="../pages/admin.html?username="+input_username;
+                    return;                    
+               }                             
+          }
+          console.log("no match");       
+     }
+     
+})
